5266d525b5830f7ff25d5d04390bb72cd02041ed
Calcificating <leonardo.pezelj17@gmail.com>
2026-07-08 09:43:21 +0200

    demo scene setup nd update simulation tick (seed and time)

--- experiments/main.cpp
+++ experiments/main.cpp
@@ -1,9 +1,9 @@
 #include "world.h"
 #include "sim.h"
+#include "scene.h"
 #include <iostream>
 
 void printWorld(World& w) {
-    // downsample a bit so it fits in a terminal, just skip every other row/col
     for (int y = 0; y < HEIGHT; y += 2) {
         for (int x = 0; x < WIDTH; x += 2) {
             char c = '.';
@@ -23,32 +23,16 @@ void printWorld(World& w) {
 
 int main() {
     World w;
+    setupDemoScene(w);
 
-    // sand block
-    for (int y = 5; y < 20; y++)
-        for (int x = 20; x < 50; x++)
-            w.cur[y * WIDTH + x].type = TYPE_SAND;
-
-    // water pool near the bottom
-    for (int y = HEIGHT - 15; y < HEIGHT - 2; y++)
-        for (int x = 10; x < 100; x++)
-            w.cur[y * WIDTH + x].type = TYPE_WATER;
-
-    // small fire source
-    for (int y = 40; y < 43; y++)
-        for (int x = 60; x < 63; x++) {
-            w.cur[y * WIDTH + x].type = TYPE_FIRE;
-            w.cur[y * WIDTH + x].life = 50;
-        }
-
+    uint32_t seed = 1337;
     int totalTicks = 200;
     for (int t = 0; t < totalTicks; t++) {
-        simTick(w);
+        simTick(w, seed, t);
         if (t % 20 == 0) {
-            std::cout << "tick " << t << "\n";
+            std::cout << "tick " << t << " hash=" << hashWorld(w) << "\n";
             printWorld(w);
         }
     }
-
     return 0;
 }
