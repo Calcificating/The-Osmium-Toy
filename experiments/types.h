83db6158201540d87afb36b88aec5db969820786
Calcificating <leonardo.pezelj17@gmail.com>
2026-07-08 09:50:09 +0200

    particle struct

--- experiments/types.h
+++ experiments/types.h
@@ -1,8 +1,6 @@
 #pragma once
 #include <cstdint>
 
-// just hardcoding these for now, should probably read from a cfg file
-// or argv at some point but whatever, its a spike
 const int WIDTH = 120;
 const int HEIGHT = 90;
 
@@ -16,12 +14,9 @@ enum ElemType {
     NUM_TYPES
 };
 
-// loosely mirrors the real Particle struct fields (type/life/temp/tmp)
-// but flattened down since we dont need the full float x,y stuff for
-// a grid based toy sim
 struct Particle {
     int type = TYPE_EMPTY;
     float temp = 22.0f;
     int life = 0;
-    int tmp = 0; // scratch field, reused for different stuff per element, same hack as upstream tbh
+    int tmp = 0;
 };
